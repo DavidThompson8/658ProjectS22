2841e92d432f94770694334b7a9f2978262ce2ed
DavidThompson8 <thompsondavidj8@gmail.com>
2022-04-18 15:19:11 -0400

    AccountTest

---
+++ src/test/java/AccountTest.java
@@ -0,0 +1,69 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountTest {
+
+    Account a1;
+
+    @Before
+    public void setUp() throws Exception {
+        a1 = new Account(4, 4, 40);
+    }
+
+
+    @Test
+    public void getPan() {
+        assertEquals(4,a1.getPan());
+    }
+
+    @Test
+    public void getPin() {
+        assertEquals(4, a1.getPin());
+    }
+
+    @Test
+    public void getBalance() {
+        assertEquals(40.00, a1.getBalance(), .5);
+    }
+
+    @Test
+    public void deposit() {
+        a1.deposit(12);
+        assertEquals(52.00, a1.getBalance(), .5);
+    }
+
+    @Test
+    public void deposit2() {
+        a1.deposit(1.20);
+        assertEquals(41.20, a1.getBalance(), .5);
+    }
+
+    @Test
+    public void withdraw() {
+        a1.withdraw(10);
+        assertEquals(30, a1.getBalance(), .5);
+    }
+
+    @Test
+    public void withdraw2() {
+        a1.withdraw(0.10);
+        assertEquals(39.90, a1.getBalance(), .5);
+    }
+
+    @Test
+    public void generateAccounts() {
+        Account[] a = Account.generateAccounts();
+        assertEquals(1, a[0].getPan());
+        assertEquals(2, a[1].getPan());
+        assertEquals(3, a[2].getPan());
+        assertEquals(123, a[0].getPin());
+        assertEquals(123, a[1].getPin());
+        assertEquals(123, a[2].getPin());
+        assertEquals(0, a[0].getBalance(), .5);
+        assertEquals(1000, a[1].getBalance(), .5);
+        assertEquals(15.17, a[2].getBalance(), .5);
+    }
+}
